45e7ff7ecce6bb2ee24e8c779f83efa4df3efe73
Saxena <saxenap@aetna.com>
2024-09-04 11:47:25 -0500

    Adding player section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 
 function App() {
-  
-
   return (
-    <p>Coming soon</p>
+    <main>
+      <div id='game-container'>
+        <ol id='players'>
+          <li>
+            <span className='player-name'>Player 1</span>
+            <span className='player-symbol'>X</span>
+          </li>
+          <li>
+            <span className='player-name'>Player 2</span>
+            <span className='player-symbol'>O</span>
+          </li>
+        </ol>
+        Game Board
+      </div>
+      Logs
+    </main>
   )
 }
 
