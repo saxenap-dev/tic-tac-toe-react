3399233cc9819766087a2872eaf8863c8d6323ad
Saxena <saxenap@aetna.com>
2024-09-05 11:16:03 -0500

    working with state for edit player

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,14 +1,24 @@
 import { useState } from "react";
 
 export default function Player({name, symbol}) {
+    const [isEditing, setIsEditing] = useState(false);
+    let playerName = <span className='player-name'>{name}</span>;
+
+    function handleEditClick() {
+        setIsEditing(true);
+    }
+
+    if(isEditing) {
+        playerName = <input type='text' required/>
+    }
 
     return (
         <li>
             <span className='player'>
-                <span className='player-name'>{name}</span>
+                {playerName}
                 <span className='player-symbol'>{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleEditClick}>Edit</button>
         </li>
     );
 }
