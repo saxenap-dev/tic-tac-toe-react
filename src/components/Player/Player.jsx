7e9818c1fb5fb36dde0f3874abf9f9e50eabc3aa
Saxena <saxenap@aetna.com>
2024-09-05 11:06:39 -0500

    Player component
    Moving code to Player component

---
+++ src/components/Player/Player.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+export default function Player({name, symbol}) {
+
+    return (
+        <li>
+            <span className='player'>
+                <span className='player-name'>{name}</span>
+                <span className='player-symbol'>{symbol}</span>
+            </span>
+            <button>Edit</button>
+        </li>
+    );
+}
